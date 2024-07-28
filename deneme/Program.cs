3d5eee27b632777e8b86581ca401d7474c03e10c
zeyneperdogan <neperdogan@gmail.com>
2024-07-28 10:43:12 +0300

    c# second answer

---
+++ deneme/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+namespace deneme
+{
+    internal class Program
+    {
+        [DllImport("Dll3.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int Multiply(int a, int b);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            int y = int.Parse(Console.ReadLine());
+
+            int result = Multiply(x, y);
+
+            Console.WriteLine($"Conclusion: {result}");
+            Console.ReadKey();
+        }
+    }
+}
